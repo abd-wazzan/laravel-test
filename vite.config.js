d7251d2f31697e92e4dd6985e760bda427bcb0d0
Abdulrahman Wazzan <abdulrahmanwazzan.pro@gmail.com>
2023-05-01 22:43:58 +0400

    Write Login Page

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,72 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+export default defineConfig(({command, mode, ssrBuild}) => {
+    if (command === 'serve') {
+        return {
+            server: {
+                host: '0.0.0.0',
+                hmr: {
+                    // host: 'localhost',
+                    host: 'laravel.test',
+                },
+                watch: {
+                    usePolling: true,
+                },
+            },
+            build: {
+                manifest: true,
+                outDir: 'public/build',
+                rollupOptions: {
+                    input: [
+                        'resources/sass/app.scss',
+                        'resources/js/app.js',
+                    ],
+                },
+                target: ['esnext']
+            },
+            plugins: [
+                laravel({
+                    input: [
+                        'resources/sass/app.scss',
+                        'resources/js/app.js',
+                    ],
+                    refresh: true,
+                }),
+            ]
+        }
+    } else {
+        return {
+            server: {
+                host: '0.0.0.0',
+                hmr: {
+                    // host: 'localhost',
+                    host: 'laravel.test',
+                },
+                watch: {
+                    usePolling: true,
+                },
+            },
+            build: {
+                manifest: true,
+                outDir: 'public/build',
+                rollupOptions: {
+                    input: [
+                        'resources/sass/app.scss',
+                        'resources/js/app.js',
+                    ],
+                },
+                target: ['esnext']
+            },
+            plugins: [
+                laravel({
+                    input: [
+                        'resources/sass/app.scss',
+                        'resources/js/app.js',
+                    ],
+                    refresh: true,
+                }),
+            ]
+        }
+    }
 });
