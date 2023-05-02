0d60398fcf8e1f009e41748ce861972faec08124
Abdulrahman Wazzan <abdulrahmanwazzan.pro@gmail.com>
2023-05-02 17:08:34 +0400

    Write Store User Endpoint Test

---
+++ tests/Feature/UserControllerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Models\User;
+
+it('can create a user', function (): void {
+
+    $userDefinition = User::factory()->definition();
+    $this->actingAs(User::factory()->create());
+    $this->assertAuthenticated('web');
+    $data = [
+        'prefixname' => $userDefinition['prefixname'],
+        'firstname' => $userDefinition['firstname'],
+        'middlename' => $userDefinition['middlename'],
+        'lastname' => $userDefinition['lastname'],
+        'suffixname' => $userDefinition['suffixname'],
+        'email' => $userDefinition['email'],
+        'password' => 'password',
+        'password_confirmation' => 'password',
+        'type' => $userDefinition['type'],
+    ];
+    $this->assertDatabaseCount('users', 1);
+    $this->post(route('users.store'), $data)->assertStatus(302);
+    $this->assertDatabaseHas('users', ['email' => $userDefinition['email']]);
+    $this->assertDatabaseCount('users', 2);
+});
+
+
